1c07b5cddf028219a00ed68907689c3bf21b016a
DAWAR <dawarpratham8@gmail.com>
2026-01-09 22:59:01 +0530

    created sign up form using sign in form

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,6 +1,11 @@
 import Header from "./Header"
 import { bgImg } from "../Utils/Const"
+import { useState } from "react";
 const Login = () => {
+    const [isSignedIn, setIsSignedIn] = useState(true);
+    const toggleForm = () => {
+        setIsSignedIn(!isSignedIn);
+    }
     return (
         <div>
             <Header/>
@@ -8,10 +13,30 @@ const Login = () => {
                 <img src={bgImg} alt="bgImg" />
             </div>
             <form className="w-3/12 absolute bg-black/75 text-white my-30 mx-auto left-0 right-0  p-10 rounded-lg">
-                <h1 className="text-3xl font-bold my-4" >Sign In</h1>
-                <input type="text" placeholder="Email Address" className="p-4 my-4 mx-auto w-full bg-gray-700 rounded-lg"/>
-                <input type="password" placeholder="Password" className="p-4 my-4 mx-auto w-full bg-gray-700 rounded-lg"/>
-                <button className="p-6 my-4 mx-auto bg-red-900 rounded-lg w-full">Sign In</button>
+                <h1 className="text-3xl font-bold my-4" >{isSignedIn ? "Sign In" : "Sign Up"}</h1>
+                {!isSignedIn && (<input type="text" 
+                    placeholder="Full Name" 
+                    className="p-4 
+                                my-4 
+                                mx-auto 
+                                w-full 
+                                cursor-pointer 
+                                bg-gray-700 
+                                rounded-lg"/>)}
+                <input type="text" 
+                    placeholder="Email Address" 
+                    className="p-4 
+                                my-4 
+                                mx-auto 
+                                w-full 
+                                cursor-pointer 
+                                bg-gray-700 
+                                rounded-lg"/>
+                <input type="password" placeholder="Password" className="p-4 my-4 mx-auto w-full cursor-pointer bg-gray-700 rounded-lg"/>
+                <button className="p-6 my-4 mx-auto bg-red-900 rounded-lg w-full cursor-pointer">{isSignedIn ? "Sign In" : "Sign Up"}</button>
+                <p className="my-4 cursor-pointer" onClick={toggleForm}>
+                    {isSignedIn ? "New to Netflix? Sign Up now" : "Already have an account? Sign In"}
+                    </p>
             </form>
         </div>
     )
