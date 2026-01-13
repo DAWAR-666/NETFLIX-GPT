d3e5e263016a1900d3af6a242399a1c6e7048f03
DAWAR <dawarpratham8@gmail.com>
2026-01-13 12:50:19 +0530

    added options to const and fetched now playing movie list

--- src/Components/Browse.tsx
+++ src/Components/Browse.tsx
@@ -1,8 +1,18 @@
 
+import { useEffect } from "react";
+import { APIoptions } from "../Utils/Const";
 import Header from "./Header"
 
 
 const Browse = () => {
+  const getNowPlaying=async()=>{
+    const data=await fetch ('https://api.themoviedb.org/3/movie/now_playing?page=1',APIoptions);
+    const jsonData=await data.json();
+    console.log(jsonData);
+  }
+  useEffect(()=>{
+    getNowPlaying();
+  },[])
   
   return (
     
--- src/Utils/Const.ts
+++ src/Utils/Const.ts
@@ -1,3 +1,10 @@
 export const bgImg: string = "https://assets.nflxext.com/ffe/siteui/vlv3/ce462eb6-4d7f-4c9a-9f61-93cb535a64fd/web/IN-en-20260105-TRIFECTA-perspective_5ec818ea-11f4-4bff-a409-8f36e9f9a1e2_medium.jpg";
 export const nLogo:string="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const userLogo:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRKNdKRIgbcMkyGq1cQeq40IA-IQS-FDWnTQ&s";
+export const APIoptions = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzllMjQ0NjgzODFlYTg5NGFiNWYxMzZhNmZhNjcyOCIsIm5iZiI6MTc2ODI4NzQzNy4wMzUsInN1YiI6IjY5NjVlY2NkZDg1MzUzYjBlZDk4YzI1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4xEUrP8S1KpoXVH1yFW-8Oz5nppp_ZLNHf4DGyzgW9s'
+  }
+};
