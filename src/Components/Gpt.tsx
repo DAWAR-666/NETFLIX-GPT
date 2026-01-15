aed77922eb0919b5dcfab20476650677bdc64572
DAWAR <dawarpratham8@gmail.com>
2026-01-15 22:09:51 +0530

    created searchbar with css

--- src/Components/Gpt.tsx
+++ src/Components/Gpt.tsx
@@ -1,3 +1,4 @@
+import { bgImg } from "../Utils/Const"
 import GptSearchBar from "./GptSearchBar"
 import GptSuggestions from "./GptSuggestions"
 
@@ -5,8 +6,13 @@ import GptSuggestions from "./GptSuggestions"
 const Gpt = () => {
   return (
     <div>
+        <div className="absolute ">
+            <img src={bgImg} alt="bgImg" className="h-screen w-screen" />
+        </div>
+        <div className="relative bg-linear-to-t from-black/30 to-black/70 h-screen">
         <GptSearchBar />
         <GptSuggestions />
+        </div>
     </div>
   )
 }
--- src/Components/GptSearchBar.tsx
+++ src/Components/GptSearchBar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
 
 const GptSearchBar = () => {
   return (
-    <div>GptSearchBar</div>
+    <div className="pt-[10%] flex justify-center ">
+        
+        <form className=" p-4 grid grid-cols-12 gap-1 rounded-e-full rounded-s-full w-1/2 z-20 ">
+            <input 
+                type="text" 
+                placeholder="What would you like to watch today?" 
+                className=" bg-gray-400 rounded-l-full p-2 col-span-9 text-black cursor-pointer "
+            />
+            <button
+            type="button"
+                className="rounded-r-full p-2 bg-red-800 text-white col-span-3 cursor-pointer"
+            >
+                Search
+            </button>
+        </form>
+        
+    </div>
   )
 }
 
