c3391663c0b2b6e603844ee722692a29e0c58bfa
DAWAR <dawarpratham8@gmail.com>
2026-01-15 15:44:06 +0530

    added video title along with its css

--- src/Components/MainCont.tsx
+++ src/Components/MainCont.tsx
@@ -4,15 +4,15 @@ import VidBg from "./VidBg";
 
 const MainCont = () => {
     const movies=useSelector(store=>store.movies?.nowPlaying);
-    if (!movies) return <div>Loading...</div>;
+    if (!movies || movies.length === 0) return <div>Loading...</div>;
     const randomIndex = Math.floor(Math.random() * movies.length);
     const selectedMovie = movies[randomIndex];
     console.log("Selected Movie:", selectedMovie);
-    const {title,original_title, overview} = selectedMovie;
+    const {title, overview} = selectedMovie;
     
   return (
     <div>
-        <VidTitle title={title} overview={overview} org_title={original_title}/>
+        <VidTitle title={title} overview={overview} />
         <VidBg />
     </div>
   )
--- src/Components/VidTitle.tsx
+++ src/Components/VidTitle.tsx
@@ -1,6 +1,33 @@
-const VidTitle = () => {
+import { useState } from "react";
+
+const VidTitle = ({title, overview}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const isLongText = overview?.length > 100;
     return (
-        <div>VidTitle</div>
+        <div className="pt-38 px-8 space-y-4 ">
+            <h2 className="text-3xl md:text-5xl">{title}</h2>
+            <div className="w-1/3 relative"> 
+                
+                <p className={`text-sm md:text-lg ${!isExpanded ? "line-clamp-2" : ""}`}>
+                    {overview}
+                </p>
+
+                
+                {isLongText && (
+                    <button 
+                        onClick={() => setIsExpanded(!isExpanded)}
+                        className="text-black hover:text-gray-400 font-bold cursor-pointer transition-colors"
+                    >
+                        {isExpanded ? " (show less)" : "(show more)"}
+                    </button>
+                )}
+            </div>
+            <div className="space-x-4 text-lg md:text-xl">
+                <button className="bg-white text-black rounded-lg border-2 border-black p-1 px-5 cursor-pointer ">▶️Play</button>
+                <button className="bg-gray-500/75 text-white rounded-lg border-2 border-black p-1 px-5 cursor-pointer">More Info</button>
+            </div>
+            
+        </div>
     )
 }
 export default VidTitle;
